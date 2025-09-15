2a4041195ab16b10ae730d9b309eda60c063c6b2
Nandan-Neviton <nandan.avadhani@neviton.com>
2025-09-15 10:49:18 +0530

    Testing by adding two seperate file

--- tests/utils/login.js
+++ tests/utils/login.js
@@ -1,10 +1,10 @@
-export async function login(page, baseUrl, username, password) {
-    await page.goto(baseUrl);
+export async function login(page, username, password) {
+    await page.goto('https://uat.note-iq.com');
     console.log(">>> Navigated to:", page.url());
   
     await page.getByRole('textbox', { name: /email/i }).fill(username);
     await page.getByRole('textbox', { name: /password/i }).fill(password);
-    await page.getByRole('button', { name: /login/i }).click();
+    await page.getByRole('button', { name: 'LOGIN', exact: true }).click();
   
     await page.waitForURL(/dashboard|home/, { timeout: 15000 });
     console.log(">>> Logged in successfully:", await page.title());
